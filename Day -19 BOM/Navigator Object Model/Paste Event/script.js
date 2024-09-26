d0fb1f7288aed52227a2ad41702d84a817e82e5d
officialayushyadav1234 <terminal>
2024-09-26 12:49:56 +0530

    Browser Object Model

---
+++ Day -19 BOM/Navigator Object Model/Paste Event/script.js
@@ -0,0 +1,9 @@
+// window.addEventListener("paste",(e)=>{
+//     e.preventDefault()
+// })
+
+const password=document.getElementById("pass")
+
+password.addEventListener("paste",(e)=>{
+    e.preventDefault()
+})
